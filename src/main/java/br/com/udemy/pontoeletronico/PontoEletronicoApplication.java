2e066bdb60866fdf8a9f3f363868e56cd5c64698
weslyvinicius <weslyvinicius@hotmail.com>
2019-02-04 11:57:52 -0200

    commit inicial do projeto

---
+++ src/main/java/br/com/udemy/pontoeletronico/PontoEletronicoApplication.java
@@ -0,0 +1,14 @@
+package br.com.udemy.pontoeletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoEletronicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PontoEletronicoApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/br/com/udemy/pontoeletronico/PontoeletronicoApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.udemy.pontoeletronico;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PontoeletronicoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
